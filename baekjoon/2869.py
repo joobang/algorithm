47eb24ebd7a414b139e642b3b6642173f0dc6166
dojoohwan <ehwnghks@gmail.com>
2024-01-28 20:29:25 +0900

    2869 solve

---
+++ baekjoon/2869.py
@@ -0,0 +1,15 @@
+import math
+import sys
+
+A, B, V = map(int , sys.stdin.readline().split())
+
+//print(math.ceil(V/A))
+diff = A - B;
+if A == V:
+    print(1)
+else:
+    if diff == 1: 
+        print(V - A + 1)
+    else:
+        print(math.ceil((V - A)/diff) + 1)
+        
