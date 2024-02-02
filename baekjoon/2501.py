f66d3dcc2f57672c913476805660ab07418e2ba3
dojoohwan <ehwnghks@gmail.com>
2024-02-02 22:29:16 +0900

    2501 solve

---
+++ baekjoon/2501.py
@@ -0,0 +1,14 @@
+from sys import stdin
+
+N, K = map(int , stdin.readline().split())
+cnt = 0
+result = 0
+for x in range(1,N):
+    if N % x == 0:
+        cnt += 1;
+        if cnt == K:
+            result = x
+            break
+
+print(result)
+
