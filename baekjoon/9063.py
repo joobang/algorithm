826e20469b12a2eef1adf61d04989da82d532b5e
dojoohwan <ehwnghks@gmail.com>
2024-05-12 14:32:39 +0900

    9063 solve

---
+++ baekjoon/9063.py
@@ -0,0 +1,23 @@
+from sys import stdin
+n = int(stdin.readline())
+
+minx = 10000
+maxx = -10000
+miny = 10000
+maxy = -10000
+for i in range(n):
+    arr = list(map(int, stdin.readline().split()))
+    if arr[0] > maxx:
+        maxx = arr[0]
+        
+    if arr[0] < minx:
+        minx = arr[0]
+        
+    if arr[1] > maxy:
+        maxy = arr[1]
+        
+    if arr[1] < miny:
+        miny = arr[1]
+        
+print(maxx,minx,maxy,miny)
+print((maxx-minx) * (maxy-miny))
