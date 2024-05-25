ec4fe377bda44831f5dbe6c0bfd040dfb3d200a0
dojoohwan <ehwnghks@gmail.com>
2024-05-25 18:04:19 +0900

    24262 ~ 24265 solve

---
+++ baekjoon/24264.py
@@ -0,0 +1,5 @@
+from sys import stdin
+n = int(stdin.readline())
+
+print(n*n)
+print(2)
---
+++ baekjoon/24265.py
@@ -0,0 +1,7 @@
+from sys import stdin
+n = int(stdin.readline())
+
+n_list = list(range(1,n))
+
+print(sum(n_list))
+print(2)
